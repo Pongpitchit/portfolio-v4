264950d8e80be62d77b5aaded6f941bf20970ec0
Best <Pongpitchitza@gmail.com>
2026-04-13 22:12:11 +0700

    tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,11 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+    './types/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
